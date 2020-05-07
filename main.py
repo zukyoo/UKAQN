c8da84fcd80aaba5fde16c4c6114ca92bed5f2b4
Kotoki1337 <Kotoki1337@gmail.com>
2020-05-07 15:00:55 +0800

    1.0.0 release

--- main.py
+++ main.py
@@ -1,11 +1,20 @@
 from pypresence import Presence
 import pygetwindow as gw
 import win32com.client
+import requests
 import time
 import os
 
 osu_close = False
 gameOpend = False
+
+state = any
+active = any
+
+version = "1.0.0"
+
+r = requests.get('https://api.github.com/repos/Kotoki1337/AquilaRP/releases/latest')
+latest = r.json()["tag_name"]
 
 path = 'C:\Windows\System32\drivers\etc'
 os.chdir(path)
@@ -21,12 +30,19 @@ def read_hosts():
     return data
 
 def getActive():
+    global state
+    global active
     tittle = gw.getActiveWindowTitle()
     keyword = "osu!"
     keyword_full = "osu!  - "
     if keyword in str(tittle):
         if keyword_full in str(tittle):
-            state = tittle.replace("osu!  - ", "")
+            if ".osu" in str(tittle):
+                state = tittle.replace("osu!  - ", "").replace(".osu", "")
+                active = "Resolving beatmap"
+            else:
+                state = tittle.replace("osu!  - ", "")
+                active = "Getting good"
         else:
             state = "Idle"
         if state == "osu!":
@@ -34,9 +50,10 @@ def getActive():
         elif "watching" in state:
             specname = tittle.replace("osu!  -  (watching ", "").replace(")", "")
             state = f"Spectating {specname}"
+            active = "Get good"
     else:
         state = "AFK"
-    return state
+    return state, active
 
 def check_server():
     datas = read_hosts()
@@ -58,12 +75,12 @@ def check_server():
     return server
 
 def setActive():
-    state = getActive()
+    state, active = getActive()
     server = check_server()
     if state == "AFK" or state == "Idle":
         RPC.update(details = state, large_image= "aqn", large_text="TheAquila Client", small_image = server, small_text = f"Playing on {server} server")
     else:
-        RPC.update(details = state, state = "Getting good", large_image="aqn", large_text="TheAquila Cilent", small_image = server, small_text = f"Playing on {server} server")
+        RPC.update(details = state, state = active, large_image="aqn", large_text="TheAquila Cilent", small_image = server, small_text = f"Playing on {server} server")
 
 def check_exsit(process_name):
     global osu_close
@@ -91,7 +108,11 @@ def check_exsit(process_name):
 
     return osu_run
 
-print("Version: 1.0.0-prerelease\nhttps://github.com/Kotoki1337/AquilaRP")
+print(f"Version: {version}\nhttps://github.com/Kotoki1337/AquilaRP")
+if latest != version:
+    print("There is a new release on Github!")
+else:
+    print(f"The {version} is the latest release in Github!")
 
 while True:
     check_exsit("osu!.exe")
